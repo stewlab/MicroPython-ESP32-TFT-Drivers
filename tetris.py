05f7617a24f0e6480b4ef8a7877f168cd1b05000
Thom <thom@tebell.io>
2025-03-13 22:36:07 -0500

    feat: slow tetris

---
+++ tetris.py
@@ -0,0 +1,115 @@
+from ili9341 import Display, color565
+from xpt2046 import Touch
+from machine import idle, Pin, SPI
+import time
+import random
+
+class Tetris:
+    BOARD_WIDTH = 10
+    BOARD_HEIGHT = 20
+    BLOCK_SIZE = 10
+
+    COLORS = [color565(0, 0, 0), color565(255, 0, 0), color565(0, 255, 0),
+              color565(0, 0, 255), color565(255, 255, 0), color565(0, 255, 255), color565(255, 0, 255)]
+
+    SHAPES = [
+        [(0, 0), (1, 0), (0, 1), (1, 1)],  # Square
+        [(0, 0), (1, 0), (2, 0), (3, 0)],  # Line
+        [(0, 0), (0, 1), (1, 1), (2, 1)],  # L-shape
+        [(2, 0), (0, 1), (1, 1), (2, 1)],  # J-shape
+        [(1, 0), (0, 1), (1, 1), (2, 1)],  # T-shape
+        [(0, 0), (1, 0), (1, 1), (2, 1)],  # Z-shape
+        [(1, 0), (2, 0), (0, 1), (1, 1)]   # S-shape
+    ]
+
+    def __init__(self, display, spi2):
+        self.display = display
+        self.touch = Touch(spi2, cs=Pin(33), int_pin=Pin(36),
+                           int_handler=self.touchscreen_press)
+
+        self.board = [[0 for _ in range(self.BOARD_WIDTH)] for _ in range(self.BOARD_HEIGHT)]
+        self.current_piece = self.new_piece()
+        self.running = True
+
+    def new_piece(self):
+        shape = random.choice(self.SHAPES)
+        color = random.randint(1, len(self.COLORS) - 1)
+        return {'shape': shape, 'x': 4, 'y': 0, 'color': color}
+
+    def draw_board(self):
+        for y in range(self.BOARD_HEIGHT):
+            for x in range(self.BOARD_WIDTH):
+                color = self.COLORS[self.board[y][x]]
+                self.display.fill_rectangle(x * self.BLOCK_SIZE, y * self.BLOCK_SIZE, self.BLOCK_SIZE, self.BLOCK_SIZE, color)
+
+    def draw_piece(self, piece, clear=False):
+        color = self.COLORS[0] if clear else self.COLORS[piece['color']]
+        for px, py in piece['shape']:
+            self.display.fill_rectangle((piece['x'] + px) * self.BLOCK_SIZE,
+                                        (piece['y'] + py) * self.BLOCK_SIZE,
+                                        self.BLOCK_SIZE, self.BLOCK_SIZE, color)
+
+    def can_move(self, piece, dx, dy):
+        for px, py in piece['shape']:
+            nx, ny = piece['x'] + px + dx, piece['y'] + py + dy
+            if nx < 0 or nx >= self.BOARD_WIDTH or ny >= self.BOARD_HEIGHT:
+                return False
+            if ny >= 0 and self.board[ny][nx]:
+                return False
+        return True
+
+    def merge_piece(self, piece):
+        for px, py in piece['shape']:
+            self.board[piece['y'] + py][piece['x'] + px] = piece['color']
+
+    def clear_lines(self):
+        self.board = [row for row in self.board if any(cell == 0 for cell in row)]
+        while len(self.board) < self.BOARD_HEIGHT:
+            self.board.insert(0, [0] * self.BOARD_WIDTH)
+
+    def update(self):
+        self.draw_piece(self.current_piece, clear=True)
+        if self.can_move(self.current_piece, 0, 1):
+            self.current_piece['y'] += 1
+        else:
+            self.merge_piece(self.current_piece)
+            self.clear_lines()
+            self.current_piece = self.new_piece()
+            if not self.can_move(self.current_piece, 0, 0):
+                self.running = False
+
+        self.draw_piece(self.current_piece)
+        self.draw_board()
+
+    def touchscreen_press(self, x, y):
+        if x < self.display.width // 2:
+            if self.can_move(self.current_piece, -1, 0):
+                self.current_piece['x'] -= 1
+        else:
+            if self.can_move(self.current_piece, 1, 0):
+                self.current_piece['x'] += 1
+
+
+def test():
+    spi1 = SPI(1, baudrate=40000000, sck=Pin(14), mosi=Pin(13))
+    display = Display(spi1, dc=Pin(2), cs=Pin(15), rst=Pin(0))
+
+    bl_pin = Pin(21, Pin.OUT)
+    bl_pin.on()
+
+    spi2 = SPI(2, baudrate=1000000, sck=Pin(25), mosi=Pin(32), miso=Pin(39))
+
+    game = Tetris(display, spi2)
+
+    try:
+        while game.running:
+            game.update()
+            time.sleep(0.5)
+            idle()
+
+    except KeyboardInterrupt:
+        print("\nCtrl-C pressed. Exiting...")
+    finally:
+        display.cleanup()
+
+test()
